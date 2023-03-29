b05e0a9eba1bfc04a9ff54021c09d5ad1ec1e916
Aaron Cayabyab <83424930+amc0131@users.noreply.github.com>
2023-03-29 14:51:39 -0400

    Add player movement functionality
    Implemented WASD movement, jumping, first person camera, mouse look, gravity variable, and a Ground layer for GroundCheck

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float playerSpeed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundedCheck;
+    public float groundDistance = 0.6f;
+    public LayerMask groundMask;
+
+    Vector3 playerVelocity;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundedCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && playerVelocity.y < 0) {
+            playerVelocity.y = -2f;
+        }
+
+        float playerX = Input.GetAxis("Horizontal");
+        float playerZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * playerX + transform.forward * playerZ;
+
+        controller.Move(move * playerSpeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        playerVelocity.y += gravity * Time.deltaTime;
+
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
